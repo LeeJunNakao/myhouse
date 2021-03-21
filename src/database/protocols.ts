1db42ef024935609ef93d316dcc1a15fa8e93d42
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2021-03-21 15:44:22 -0300

    feat: create house repository

--- src/database/protocols.ts
+++ src/database/protocols.ts
@@ -1,5 +1,7 @@
-import { CreateHouseDto, House } from '../domain/House';
+import { CreateHouseDto, House } from "../domain/House";
 
 export interface HouseRepository {
-    create(dto: CreateHouseDto): Promise<House>
+  create(dto: CreateHouseDto): Promise<House>;
+  get(id: number | string): Promise<House[]>;
+  update(dto: House): Promise<House>;
 }
