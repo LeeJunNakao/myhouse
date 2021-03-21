a40d7b5f823bd7e1bbf723d0f1df70e3d643fe2d
Alex Lee Jun Nakao <leejun.nakao@gmail.com>
2021-03-21 18:56:51 -0300

    feat: implement getHouseByMemberId and updateHouse in house service

--- src/database/repositories/house-repo.test.ts
+++ src/database/repositories/house-repo.test.ts
@@ -1,40 +1,40 @@
-import { HouseRepository } from "./house-repo";
-import { CreateHouseDto, House } from "../../domain/House";
-import { truncateDatabase } from "../helpers/query-helper";
+import { HouseRepository } from './house-repo';
+import { CreateHouseDto } from '../../domain/House';
+import { truncateDatabase } from '../helpers/query-helper';
 
 const createHouseDto: CreateHouseDto = {
-  name: "Casa de alguém",
+  name: 'Casa de alguém',
   members: [1, 2, 3],
 };
 
-describe("House Repository", () => {
-  beforeAll(async () => await truncateDatabase());
-  afterEach(async () => await truncateDatabase());
+describe('House Repository', () => {
+  beforeAll(async() => await truncateDatabase());
+  afterEach(async() => await truncateDatabase());
 
-  test("Should insert into database successfully", async () => {
+  test('Should insert into database successfully', async() => {
     const repo = new HouseRepository();
     const house = await repo.create(createHouseDto);
     expect(house).toEqual({ ...createHouseDto, id: 1 });
   });
 
-  test("Should insert get by id successfully", async () => {
+  test('Should insert get by id successfully', async() => {
     const repo = new HouseRepository();
     const { id, name, members } = await repo.create(createHouseDto);
     const house = await repo.get(createHouseDto.members[0]);
     expect(house).toEqual([{ id, name, members }]);
   });
 
-  test("Should update successfully", async () => {
+  test('Should update successfully', async() => {
     const repo = new HouseRepository();
     const { id } = await repo.create(createHouseDto);
-    const house = await repo.update({ id, name: "Novo nome", members: [1, 2] });
-    expect(house).toEqual({ id, name: "Novo nome", members: [1, 2] });
+    const house = await repo.update({ id, name: 'Novo nome', members: [1, 2] });
+    expect(house).toEqual({ id, name: 'Novo nome', members: [1, 2] });
   });
 
-  test("Should get all houses that user owns", async () => {
+  test('Should get all houses that user owns', async() => {
     const commomMember = 1;
-    const houseDto = { name: "Casa de Fulano", members: [commomMember, 2, 3] };
-    const houseDto2 = { name: "Casa de Beltrano", members: [commomMember, 7, 8] };
+    const houseDto = { name: 'Casa de Fulano', members: [commomMember, 2, 3] };
+    const houseDto2 = { name: 'Casa de Beltrano', members: [commomMember, 7, 8] };
     const repo = new HouseRepository();
     const house1 = await repo.create(houseDto);
     const house2 = await repo.create(houseDto2);
--- src/domain/services/house.ts
+++ src/domain/services/house.ts
@@ -1,16 +1,26 @@
-import { HouseService as IHouseService} from '../protocols/services';
+import { HouseService as IHouseService } from '../protocols/services';
 import { CreateHouseDto, House } from '../House';
 import { HouseRepository } from '../../database/protocols';
 
 export class HouseService implements IHouseService {
-    private repo: HouseRepository;
+  private readonly repo: HouseRepository;
 
-    constructor(repo: HouseRepository) {
-        this.repo = repo;
-    }
-    
-    async createHouse(house: CreateHouseDto): Promise<House> {
-        const result = await this.repo.create(house);
-        return result;
-    }
+  constructor(repo: HouseRepository) {
+    this.repo = repo;
+  }
+
+  async createHouse(house: CreateHouseDto): Promise<House> {
+    const result = await this.repo.create(house);
+    return result;
+  }
+
+  async getHouseByMemberId(memberId: number | string): Promise<House[]> {
+    const result = await this.repo.get(memberId);
+    return result;
+  }
+
+  async updateHouse(house: House): Promise<House> {
+    const result = await this.repo.update(house);
+    return result;
+  }
 }
